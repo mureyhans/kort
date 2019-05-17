7a87b91b92af62ddf2291f6bc17fbd34683b1443
cahyaramadhan <36650685+cahyaramadhan@users.noreply.github.com>
2019-05-17 14:18:32 +0700

    beberapa perubahan : 1. penambahan tombol pada scene CardSelection 2. perpindahan scene setelah tombol diklik 3. dll

--- Kort - Battle of The Medieval Era/Assets/Scripts/CardsCollection.cs
+++ Kort - Battle of The Medieval Era/Assets/Scripts/CardsCollection.cs
@@ -3,8 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardsCollection : CardStorage
+public class CardsCollection : MonoBehaviour
 {
+    //https://stackoverflow.com/questions/32306704/unity-pass-data-between-scenes
+    //https://gamedev.stackexchange.com/questions/110958/unity-5-what-is-the-proper-way-to-handle-data-between-scenes
+    [SerializeField] List<Card> cards;
+    [SerializeField] Transform cardsParent;
+    [SerializeField] CardSlot[] cardSlots;
+
+
     public event Action<Card> OnCardRightClickedEvent;
     private void Awake()
     {
@@ -13,4 +20,63 @@ public class CardsCollection : CardStorage
             cardSlots[i].OnRightClickEvent += OnCardRightClickedEvent;
         }
     }
+
+    private void OnValidate()
+    {
+        if (cardsParent != null)
+            cardSlots = cardsParent.GetComponentsInChildren<CardSlot>();
+        RefreshUI();
+    }
+
+    private void RefreshUI()
+    {
+        int i = 0;
+        for (; i < cards.Count && i < cardSlots.Length; i++)
+        {
+            cardSlots[i].Card = cards[i];
+        }
+
+        for (; i < cardSlots.Length; i++)
+        {
+            cardSlots[i].Card = null;
+        }
+    }
+
+    public bool AddCard(Card card)
+    {
+        if (IsFull())
+            return false;
+
+        cards.Add(card);
+        RefreshUI();
+        return true;
+    }
+
+    public bool RemoveCard(Card card)
+    {
+        if (cards.Remove(card)) //BUG : hapus yang pertama ditemukan 
+                                // bukan ,sirkular gara2 di hapus lalu diinsert lagi
+        {
+            RefreshUI();
+            return true;
+        }
+        return false;
+    }
+
+    public bool IsFull()
+    {
+        return cards.Count >= cardSlots.Length;
+    }
+
+    public void SetCard(int index, Card card)
+    {
+        if (index <= cards.Count)
+            cards[index] = card;
+    }
+
+    public int FindIndexCard(Card card)
+    {
+        return cards.FindIndex(a => a.name == card.name);
+    }
+
 }
