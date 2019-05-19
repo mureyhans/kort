0008a343a2037a3226b8a7b4f219382e17c33e46
Han <mreyhan693@gmail.com>
2019-05-19 19:53:34 +0700

    Display Total Points per Line

--- Kort - Battle of The Medieval Era/Assets/Scripts/DropZoneMinion.cs
+++ Kort - Battle of The Medieval Era/Assets/Scripts/DropZoneMinion.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class DropZoneTrebuchet : MonoBehaviour, IDropHandler
+public class DropZoneMinion : MonoBehaviour, IDropHandler
 {
     public string zoneType;
+    public Text point;
+
+    public int pointVal = 0;
+    
     public void OnDrop(PointerEventData eventData) {
-        Debug.Log(eventData.pointerDrag.name + "was dropped on " + gameObject.name);
+        // Debug.Log(eventData.pointerDrag.name + "was dropped on " + gameObject.name);
         DragCard d = eventData.pointerDrag.GetComponent<DragCard>();
         CardDisplayBattlefield cardComponent = eventData.pointerDrag.GetComponent<CardDisplayBattlefield>();
         CardBatt card = cardComponent.card;
         if(d != null) {
             if(card.charType == zoneType){
                 d.parentToReturnTo = this.transform;
+                pointVal = pointVal + int.Parse(cardComponent.charPointText.text);
+                point.text = pointVal.ToString();
             }
         }
+    }
+
+    void Update() {
+        point.text = pointVal.ToString();
     }
 }
