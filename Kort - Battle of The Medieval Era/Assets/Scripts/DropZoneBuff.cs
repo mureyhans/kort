f6bcb8f465fa31733c82be963c2c8ed1c257298f
Han <mreyhan693@gmail.com>
2019-05-20 08:11:23 +0700

    Buff + Total Score (success)

--- Kort - Battle of The Medieval Era/Assets/Scripts/DropZoneBuff.cs
+++ Kort - Battle of The Medieval Era/Assets/Scripts/DropZoneBuff.cs
@@ -7,13 +7,23 @@ public class DropZoneBuff : MonoBehaviour, IDropHandler
 {
     public string zoneType;
     public string zoneBuffType;
-    public DropZoneMinion dropZone;
+    //public DropZoneMinion dropZone;
+    public GameObject panelBattlefield;
 
     public void OnDrop(PointerEventData eventData) {
         if(this.transform.childCount != 0) {
             var children = new List<GameObject>();
             foreach(Transform child in this.transform) children.Add(child.gameObject);
             children.ForEach(child => Destroy(child));
+        }
+        if(panelBattlefield.transform.childCount == 0)
+        {
+            panelBattlefield.GetComponent<DropZoneMinion>().multiplier = 2;
+        } else if(panelBattlefield.transform.childCount > 0)
+        {
+            int score = panelBattlefield.GetComponent<DropZoneMinion>().pointVal;
+            panelBattlefield.GetComponent<DropZoneMinion>().multiplier = 2;
+            panelBattlefield.GetComponent<DropZoneMinion>().pointVal = score * 2;
         }
         // Debug.Log(eventData.pointerDrag.name + "was dropped on " + gameObject.name);
 
--- Kort - Battle of The Medieval Era/Assets/Scripts/DropZoneMinion.cs
+++ Kort - Battle of The Medieval Era/Assets/Scripts/DropZoneMinion.cs
@@ -10,17 +10,20 @@ public class DropZoneMinion : MonoBehaviour, IDropHandler
     public Text point;
 
     public int pointVal = 0;
+    public int multiplier = 1;
     
     public void OnDrop(PointerEventData eventData) {
         // Debug.Log(eventData.pointerDrag.name + "was dropped on " + gameObject.name);
         DragCard d = eventData.pointerDrag.GetComponent<DragCard>();
         CardDisplayBattlefield cardComponent = eventData.pointerDrag.GetComponent<CardDisplayBattlefield>();
         CardBatt card = cardComponent.card;
+        int cardScore = card.charPoint;
         if(d != null) {
             if(card.charType == zoneType){
                 d.parentToReturnTo = this.transform;
-                pointVal = pointVal + int.Parse(cardComponent.charPointText.text);
+                pointVal = pointVal + (int.Parse(cardComponent.charPointText.text) * multiplier);
                 point.text = pointVal.ToString();
+               // TotalPoint.totalScore += cardScore;
             }
         }
     }
