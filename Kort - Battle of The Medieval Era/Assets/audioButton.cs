a649eee8d5035f8b576a3621841638f919723fad
Regawa <regawa04@gmail.com>
2019-05-20 22:26:32 +0700

    Add Sounds

---
+++ Kort - Battle of The Medieval Era/Assets/audioButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioButton : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+    public void HoverSound(){
+        myFx.PlayOneShot(hoverFx);
+    }
+    public void ClickSound(){    
+        myFx.PlayOneShot(clickFx);
+    }
+}
