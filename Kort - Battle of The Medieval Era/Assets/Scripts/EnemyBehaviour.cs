b65ecfe925fa4871f060bbb35e9b0f043c050234
Han <mreyhan693@gmail.com>
2019-05-27 21:23:32 +0700

    Hided Enemy Cards

---
+++ Kort - Battle of The Medieval Era/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var children = new List<GameObject>();
+        foreach (Transform child in this.transform) children.Add(child.gameObject);
+        children.ForEach(child => child.SetActive(false));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
